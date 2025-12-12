b95b79f668a58ca7a155e2628291e5817d6af8e1
AnnePerera <anneperera008@gmail.com>
2025-12-12 20:50:38 +0530

    Add UserRepository.java in repository package

---
+++ backend/src/main/java/com/agrilinker/backend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.agrilinker.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.agrilinker.backend.model.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
