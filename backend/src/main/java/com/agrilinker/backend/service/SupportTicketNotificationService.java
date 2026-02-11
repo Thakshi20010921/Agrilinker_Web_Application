0f74f6a6a4a748383ced8767548ac7ee77697da9
AnnePerera <anneperera008@gmail.com>
2026-02-11 12:53:07 +0530

    deletion of notification

--- backend/src/main/java/com/agrilinker/backend/service/SupportTicketNotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.agrilinker.backend.service;
-
-import com.agrilinker.backend.model.SupportTicketNotification;
-import java.util.List;
-
-public interface SupportTicketNotificationService {
-    SupportTicketNotification createNotification(SupportTicketNotification notification);
-    List<SupportTicketNotification> getNotificationsByTicketId(String ticketId);
-}
