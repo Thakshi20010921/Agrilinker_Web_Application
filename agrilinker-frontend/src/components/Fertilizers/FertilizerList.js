1811f08771e3a8529869edd9c0b64e7996678760
Dewmini Jayawardhana <kavindidewmini7@gmail.com>
2026-02-13 09:20:24 +0530

    Remove edit link from public fertilizer marketplace

--- agrilinker-frontend/src/components/Fertilizers/FertilizerList.js
+++ agrilinker-frontend/src/components/Fertilizers/FertilizerList.js
@@ -272,13 +272,7 @@ export default function FertilizerList() {
               </div>
 
               <div className="flex justify-between mt-4">
-                <Link
-                  to={`/fertilizers/edit/${id}`}
-                  className="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600 transition"
-                >
-                  Edit
-                </Link>
-
+          
                 <button
                   type="button"
                   onClick={() => handleBuy(f)}
